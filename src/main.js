c19486c8ec1b6639ebcbf86b22026fd8c12433d1
xichuanwushao <avatarwx@163.com>
2023-10-30 14:02:49 +0800

    baseUrl和minioUrl添加

--- src/main.js
+++ src/main.js
@@ -91,11 +91,11 @@ app.config.globalProperties.$echarts = echarts //设置全局变量$echarts
 
 
 //后端项目的URL根路径
-let baseUrl = "http://localhost:8092/hospital-api"
+let baseUrl = "http://192.168.3.150:8092/hospital-api"
 app.config.globalProperties.$baseUrl = baseUrl //设置全局变量$baseUrl
 
 //Minio服务器地址
-let minioUrl = "http://Minio服务器IP:9000/hospital"
+let minioUrl = "http://192.168.3.151:9000/hospital"
 app.config.globalProperties.$minioUrl = minioUrl
 
 
